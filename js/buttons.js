063c22f1b0ffd8e3f9d0c289180f80bae0d25cff
Walrusking16 <walruskinggaming@gmail.com>
2020-10-17 00:21:43 -0400

    JS: Fixed bug preventing waves from working everywhere, added init func

--- js/buttons.js
+++ js/buttons.js
@@ -1,16 +1,46 @@
-$(() => {
-	$(".wave").on("click", (elem) => {
+import {wkfe} from './class';
+
+function getWaveColor(elem){
+	const classes = elem.classList;
+
+	let match = null
+
+	classes.forEach((_class) => {
+		if (_class.includes("wave-")){
+			match = _class.replace("wave-", "")
+		}
+	});
+
+	return match
+}
+
+wkfe.InitButtions = () => {
+	$(".btn.wave").off("click");
+	$(".btn.wave").on("click", (elem) => {
 		let x = elem.clientX - elem.target.offsetLeft;
-		let y = elem.clientY - elem.target.offsetTop;
+		let y = elem.clientY - elem.target.offsetTop + window.pageYOffset;
+
+		let wave_color = getWaveColor(elem.toElement);
+		if (wave_color !== null){
+			wave_color = " " + wave_color;
+		}
+		else {
+			wave_color = "";
+		}
 
 		let ripples = document.createElement('span');
+
 		$(ripples).css({
 			left: x + "px",
 			top: y + "px"
-		}).addClass("wave-temp");
+		}).addClass("wave-temp" + wave_color);
 		elem.toElement.appendChild(ripples);
 		setTimeout(() =>{
 			ripples.remove();
 		}, 1000)
 	});
+}
+
+$(() => {
+	wkfe.InitButtions();
 });
